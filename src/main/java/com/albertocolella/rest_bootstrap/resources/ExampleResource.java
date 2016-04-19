4ef30aed8f9c3f2964df73b47d5ed5eaa78425eb
Alberto <kongoji@gmail.com>
2016-04-19 23:06:29 +0200

    DB insert/fetch

--- src/main/java/com/albertocolella/rest_bootstrap/resources/ExampleResource.java
+++ src/main/java/com/albertocolella/rest_bootstrap/resources/ExampleResource.java
@@ -3,8 +3,10 @@ package com.albertocolella.rest_bootstrap.resources;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -22,21 +24,36 @@ public class ExampleResource extends DefaulResource {
 	@GET
 	@Path("world")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Example> fetchAllB() {
+	// public List<Example> fetchAllB() {
+	public Response fetchAll() {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();  
         Session session = sessionFactory.openSession();
 		session.beginTransaction();
         List<Example> examples = session.createQuery("from Example").list();  
         session.getTransaction().commit();
-        session.close();  /**/
-        return examples;
-       /* if(examples!=null && examples.size()>0){
+        session.close();
+        if(examples!=null && examples.size()>0){
+        	GenericEntity<List<Example>> entities = new GenericEntity<List<Example>>(examples) {};
 		return Response.status(Response.Status.OK)
-				.entity(examples)
+				.entity(entities)
 				.build();
         } else {
         	return Response.status(Response.Status.NO_CONTENT)
         			.build();
-        }*/
+        }
+	}
+	
+	@POST
+	@Path("world")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response insert(Example e) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();  
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save( e );
+		session.getTransaction().commit();
+		session.close();
+		return Response.status(Response.Status.CREATED)
+				.build();
 	}
 }
